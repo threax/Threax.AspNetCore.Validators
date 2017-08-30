43613ab415dffe5556aeccbed57365a29e04f7b2
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-30 09:34:00 -0400

    Created a library for custom validators.

---
+++ EmailAddressListAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Threax.AspNetCore.Validators
+{
+    public class EmailAddressListAttribute : ValidationAttribute
+    {
+        private EmailAddressAttribute emailAddressAttribute = new EmailAddressAttribute();
+
+        public EmailAddressListAttribute()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+            var emails = value as IEnumerable<String>;
+            if (emails != null)
+            {
+                foreach (var email in emails)
+                {
+                    if (!emailAddressAttribute.IsValid(email))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            throw new InvalidOperationException($"Cannot validate email address list, type was {value.GetType().Name}, must be an IEnumerable<String>.");
+        }
+    }
+}
